d7fb3e106c8a351dcbadbfd6cf83503fb5868ee0
anqalah <evodark7@gmail.com>
2025-05-15 15:15:50 +0800

    PERBAIKI MODULE

--- routes/SheetRoute.js
+++ routes/SheetRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import SheetController from "../controllers/SheetController";
+import SheetController from "../controllers/SheetController.js";
 
 const router = express.Router();
 
